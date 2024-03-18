8c7be6f1514686c8d1ecb8244898acacff8b75c3
DavidCardenas0403 <davidcardenasrafael03@gmail.com>
2024-03-18 17:30:43 +0100

    tailwind and prime vue added

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
 <template>
+  <h1 class="bg-blue-300">Book2Stay</h1>
+  <Button label="Check" />
   <div>
     <NuxtWelcome />
   </div>
 </template>
+
+<style>
+@import url("primevue/resources/themes/aura-light-green/theme.css");
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  css: [
+    "~/assets/css/main.css",
+    "primevue/resources/themes/aura-light-blue/theme.css",
+  ],
+  modules: ["nuxt-primevue"],
+  primevue: {
+    usePrimeVue: true,
+    options: {
+      ripple: true,
+    },
+    components: {
+      include: "*",
+    },
+    cssLayerOrder: "tailwind-base, primevue, tailwind-utilities",
+  },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+});
