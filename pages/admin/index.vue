e95a5c923f0ef07f17131c2f62a5d2c40b01b44d
Dani Gracia <danigraciaquiroga@gmail.com>
2024-03-20 16:58:36 +0100

    admin layout

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,8 +1,26 @@
 <template>
-    <aside class="bg-blue">
-        aside
-    </aside>
-    <div>
-        <Button label="hola" />
+    <div class="flex min-h-screen">
+        <aside
+            class="bg-zinc-800 text-gray-50 w-1/6 min-h-screen flex flex-col gap-6 p-8">
+            <h1 class="font-bold text-xl text-center">Admin
+            </h1>
+
+            <section>
+                <NuxtLink to="/admin/propiedades">
+                    <font-awesome-icon
+                        icon="fa-solid fa-house" />
+                    Propiedades
+                </NuxtLink>
+            </section>
+        </aside>
+        <div class="w-full bg-gray-200">
+            <header class="bg-zinc-800 text-gray-50 p-4 flex justify-end">
+                <span>Hola Dani</span>
+            </header>
+            <main class="p-8  container">
+                <Button label="hola" />
+            </main>
+
+        </div>
     </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./components/**/*.{js,vue,ts}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./app.vue",
-    "./error.vue",
-  ],
-  theme: {
-    colors: {
-      primary: "#3e9681",
+    content: [
+        "./components/**/*.{js,vue,ts}",
+        "./layouts/**/*.vue",
+        "./pages/**/*.vue",
+        "./plugins/**/*.{js,ts}",
+        "./app.vue",
+        "./error.vue",
+    ],
+    theme: {
+
+        extend: {
+            colors: {
+                primary: "#3e9681",
+            },
+        },
     },
-    extend: {},
-  },
-  plugins: [],
+    plugins: [],
 };
