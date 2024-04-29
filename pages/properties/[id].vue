c6a47af1ae55b0ecd0e887ddaaf4d29d6f258ff0
DavidCardenasITB <davidcardenasrafael03@gmail.com>
2024-04-29 16:57:03 +0200

    Property detail booking information and modal

--- pages/properties/[id].vue
+++ pages/properties/[id].vue
@@ -28,8 +28,8 @@
         </div>
       </div>
     </section>
-    <section class="grid grid-cols-2 gap-12 mt-8">
-      <section class="flex flex-col gap-8">
+    <section class="grid grid-cols-5 gap-12 mt-8">
+      <section class="flex flex-col gap-8 col-span-3">
         <h3>{{ getPropertyText(property)?.name }}</h3>
         <p class="text-primary text-4xl">
           {{ property.price }}€
@@ -40,8 +40,75 @@
         <div v-html="getPropertyText(property)?.description"></div>
       </section>
 
-      <div class="bg-blue-300"></div>
+      <div class="bg-gray-200 py-8 px-6 gap-2 grid grid-cols-2 col-span-2">
+        <InputGroup>
+          <InputGroupAddon>
+            <i class="pi pi-calendar"></i>
+          </InputGroupAddon>
+          <FloatLabel>
+            <Calendar
+              v-model="data.dates"
+              selectionMode="range"
+              dateFormat="dd/mm/yy"
+              class="custom-calendar"
+            />
+            <label class="text-neutral-400" for="booking_dates"
+              >Booking dates</label
+            >
+          </FloatLabel>
+        </InputGroup>
+        <div class="grid grid-cols-2 gap-2">
+          <InputGroup class="flex-1">
+            <InputGroupAddon>
+              <i class="pi pi-user"></i>
+            </InputGroupAddon>
+            <FloatLabel>
+              <InputNumber
+                id="adults"
+                v-model="data.adults"
+                for="adults"
+                showButtons
+                :min="0"
+              />
+              <label class="text-neutral-400" for="adults">Adults</label>
+            </FloatLabel>
+          </InputGroup>
+
+          <InputGroup class="flex-1">
+            <InputGroupAddon>
+              <i class="pi pi-user"></i>
+            </InputGroupAddon>
+            <FloatLabel>
+              <InputNumber
+                v-model="data.children"
+                id="children"
+                for="children"
+                showButtons
+                :min="0"
+              />
+              <label class="text-neutral-400" for="children">Children</label>
+            </FloatLabel>
+          </InputGroup>
+        </div>
+
+        <Button
+          type="button"
+          label="Book"
+          class="bg-primary-normal col-span-2"
+          :loading="loading"
+          @click="modalData.visible = true"
+        />
+      </div>
     </section>
+    <Dialog
+      header="Your book"
+      v-model:visible="modalData.visible"
+      modal
+      position="center"
+      :style="{ width: '80%', height: '80%' }"
+    >
+      Hola
+    </Dialog>
   </div>
 
   <ImagesGallery
@@ -61,6 +128,17 @@ import ImagesGallery from '~/components/properties/ImagesGallery.vue'
 import logo from '../assets/images/country-club.svg'
 import LangSwitcher from '~/components/LangSwitcher.vue'
 import { BACKEND_URL } from '~/CONSTS'
+
+const data = reactive({
+  dates: '',
+  adults: 0,
+  children: 0,
+})
+
+const modalData = reactive({
+  visible: false,
+  step: 1,
+})
 
 const property = ref(null)
 const imagesGalleryShown = ref(false)
